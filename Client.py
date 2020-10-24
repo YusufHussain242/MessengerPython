2c4b4b198b8ad2448dc9f6218caec82375688bf9
Yusuf Hussain <Yusuf.MH242@gmail.com>
2020-10-24 19:28:58 +0000

    Added Extra networking

--- Client.py
+++ Client.py
@@ -3,8 +3,12 @@ import socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.connect((socket.gethostname(), 7777))
 
+fullMessage = ""
 while True:
     message = serverSocket.recv(8)
-    print(message.decode("utf-8"))
+    if len(message) == 0:
+        break
+    fullMessage.append(message)
 
+print(fullMessage)
 input()
