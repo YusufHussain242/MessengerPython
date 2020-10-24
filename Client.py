61996b1dd5e90c75bc885a5b0932fd83d16339a8
Yusuf Hussain <Yusuf.MH242@gmail.com>
2020-10-24 20:11:48 +0100

    Test Commit

--- Client.py
+++ Client.py
@@ -4,14 +4,5 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.connect((socket.gethostname(), 7777))
 
 while True:
-  fullMessage = ""
-  
-  while True:
     message = serverSocket.recv(8)
-    if len(message) == 0:
-      break;
-    fullMessage.append(message)
-
-  print(fullMessage.decode("utf-8"))
-
-input("End")
+    print(message.decode("utf-8"))
--- Server.py
+++ Server.py
@@ -5,6 +5,6 @@ serverSocket.bind((socket.gethostname(), 7777))
 serverSocket.listen(10)
 
 while True:
-  clientSocket , address = serverSocket.accept()
-  print(f"Connection from {address} has been established!")
-  clientSocket.send(bytes("Welcome to the server!" , "utf-8"))
+    clientSocket , address = serverSocket.accept()
+    print(f"Connection from {address} has been established!")
+    clientSocket.send(bytes("Welcome to the server!" , "utf-8"))
