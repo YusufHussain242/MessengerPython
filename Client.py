456284d2c32d29c8295b0e339988738e1c5ed7a9
Yusuf Hussain <Yusuf.MH242@gmail.com>
2020-10-24 16:44:52 +0000

    Changed Client

--- Client.py
+++ Client.py
@@ -3,6 +3,13 @@ import socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.connect((socket.gethostname(), 7777))
 
-message = serverSocket.recv(1024)
-print(message.decode("utf-8"))
-input()
+while True:
+  fullMessage = ""
+  
+  while True:
+    message = serverSocket.recv(8)
+    if len(message) == 0:
+      break;
+    fullMessage.append(message)
+
+  print(fullMessage.decode("utf-8"))
