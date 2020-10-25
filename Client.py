033d7bb8ebe2b62dcee5c0c13df5c2386376bc1b
Yusuf Hussain <Yusuf.MH242@gmail.com>
2020-10-25 13:32:35 +0000

    Made some errors

--- Client.py
+++ Client.py
@@ -1,7 +1,9 @@
 import socket
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.connect((socket.gethostname(), 7777))
+serverSocket.connect((socket.gethostname() , 7777))
+
+
 
 while True:
     message = serverSocket.recv(8)
