73ed3b40d5fb3bac02ed0e7d8950d12aa229e95f
Yusuf Hussain <Yusuf.MH242@gmail.com>
2020-10-25 13:47:45 +0000

    test Client

--- Client.py
+++ Client.py
@@ -3,7 +3,6 @@ import socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.connect((socket.gethostname(), 7777))
 
-
 while True:
     message = serverSocket.recv(8)
     print(message.decode("utf-8"))
