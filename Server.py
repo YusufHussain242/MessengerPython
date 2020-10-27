b4f7a98fa59ea12fece5fb18d7dcb8fc42394ad4
Yusuf Hussain <Yusuf.MH242@gmail.com>
2020-10-27 00:22:23 +0000

    Added pickle and function functionallity

--- Server.py
+++ Server.py
@@ -1,15 +1,7 @@
 import socket
 import threading
-import struct
+import pickle
 from enum import Enum
-
-
-class T(Enum):
-    null = 0
-    string = 1
-    int = 2
-    float = 3
-    function = 4
 
 
 class V(Enum):
@@ -22,6 +14,14 @@ vars = []
 for var in V:
     vars.append(0)
 
+
+# args = [message]
+def _printMessage(args):
+    print(args[0])
+
+
+vars[V.printMessage.value] = _printMessage
+
 headerSize = 4
 clients = []
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,72 +33,32 @@ def handleDisconnect(_clientSocket, _address):
     clients.remove(_clientSocket)
 
 
-def encodeData(data, dataType, varName):
-    dataBytes = bytearray(struct.pack("i", len(data)))
-
-    dataBytes[len(dataBytes):len(dataBytes)] = \
-        dataType.value.to_bytes(1, "little")
-
-    dataBytes[len(dataBytes):len(dataBytes)] = \
+def encodeData(data, varName):
+    dataBytes = bytearray(pickle.dumps(data))
+    dataBytes[0:0] = len(dataBytes).to_bytes(4, "little") + \
         varName.value.to_bytes(1, "little")
-
-    def caseString():
-        dataBytes[len(dataBytes):len(dataBytes)] = \
-            struct.pack(f"{len(data)}s", bytes(data, "utf-8"))
-
-    def caseInt():
-        dataBytes[len(dataBytes):len(dataBytes)] = \
-            bytearray(struct.pack("i", data))
-
-    def caseFloat():
-        dataBytes[len(dataBytes):len(dataBytes)] = \
-            (struct.pack("f", data))
-
-    switch = {
-        T.string: caseString,
-        T.int: caseInt,
-        T.float: caseFloat
-    }
-
-    switch.get(dataType)()
-
     return bytes(dataBytes)
 
 
-def handleData(data, dataType, varName):
-    newData = ""
-
-    def caseString():
-        newData = struct.unpack(f"{len(data)}s", data)[0]
-        return newData.decode("utf-8")
-
-    def caseInt():
-        return struct.unpack("i", data)[0]
-
-    def caseFloat():
-        return struct.unpack("f", data)[0]
-
-    switch = {
-        T.string.value: caseString,
-        T.int.value: caseInt,
-        T.float.value: caseFloat
-    }
-    newData = switch.get(dataType)()
-
-    vars[varName] = newData
+def handleData(data, varName):
+    newData = pickle.loads(data)
+    try:
+        vars[varName](newData)
+    except TypeError:
+        vars[varName] = newData
 
     return newData
 
 
-def sendData(data, dataType, varName, _sockets):
-    dataBytes = encodeData(data, dataType, varName)
+def sendData(data, varName, _sockets):
+    dataBytes = encodeData(data, varName)
     for _socket in _sockets:
         _socket.send(dataBytes)
 
 
-def broadcastData(data, dataType, varName, exemptions, piggyback):
+def broadcastData(data, varName, exemptions, piggyback):
     if piggyback is False:
-        dataBytes = encodeData(data, dataType, varName)
+        dataBytes = encodeData(data, varName)
     else:
         dataBytes = data
 
@@ -116,28 +76,25 @@ def broadcastData(data, dataType, varName, exemptions, piggyback):
 def recieveData(_clientSocket, _address):
     print(f"{_address} has connected")
     while True:
-        # packetSize = 0
         buffer = bytearray()
         fullBuffer = bytearray()
 
         try:
-            buffer = _clientSocket.recv(headerSize+2)
-            fullBuffer[0:0] = buffer
+            buffer = _clientSocket.recv(headerSize+1)
+            fullBuffer = buffer
         except ConnectionResetError:
             handleDisconnect(_clientSocket, _address)
             break
 
         if len(buffer) > 0:
-            packetSize = struct.unpack("i", buffer[:headerSize])[0]
-            dataType = buffer[headerSize]
-            varName = buffer[headerSize+1]
+            packetSize = int.from_bytes(buffer[:headerSize], "little")
+            varName = buffer[headerSize]
 
             buffer = _clientSocket.recv(packetSize)
-            fullBuffer[len(fullBuffer):len(fullBuffer)] = buffer
-            handleData(buffer, dataType, varName)
-            print(vars[varName])
+            fullBuffer = fullBuffer + buffer
+            handleData(buffer, varName)
 
-            broadcastData(fullBuffer, T.null, V.null, [_clientSocket], True)
+            broadcastData(fullBuffer, V.null, [_clientSocket], True)
         else:
             handleDisconnect(_clientSocket, _address)
             break
@@ -157,5 +114,5 @@ listThread = threading.Thread(target=waitForConnect)
 listThread.start()
 while True:
     serverMessage = input()
-    broadcastData(serverMessage, T.string, V.printMessage, [], False)
+    broadcastData([serverMessage], V.printMessage, [], False)
 input()
